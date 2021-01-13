5d91a9b0a7c3edcfc7be253e48315ace8dc1e866
Josh Bodah <joshuabodah@gmail.com>
2021-01-13 00:45:02 -0500

    git init

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ PROJ="$(basename $(cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P))"
 sed -i.bak "s/godot-base-project/$PROJ/g" build
 sed -i.bak "s/godot-base-project/$PROJ/g" project.godot
 rm -rf build.bak project.godot.bak README.md setup.sh .git
+git init
